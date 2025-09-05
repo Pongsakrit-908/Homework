219e5c9575c7ddf02b5d457ff0e0a7fa65f5c50f
Pongsakrit-908 <0632640117tar@gmail.com>
2025-09-05 16:34:06 +0700

    หรือถ้า

---
+++ if-else/Positive.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Positive {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter your number : ");
+        int Number  = input.nextInt();
+        
+    if (Number > 0 ) {
+          System.out.print("จำนวนเต็มบวก : ");
+
+    }
+    System.out.println("จบโปรแกรม");
+    }
+}
